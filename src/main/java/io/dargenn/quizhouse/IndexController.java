c315a1c9e2ac055d973c6004c00938de80016412
Michał Dargiewicz <michaldargiewicz1@gmail.com>
2018-10-06 09:56:07 +0200

    init project

---
+++ src/main/java/io/dargenn/quizhouse/IndexController.java
@@ -0,0 +1,16 @@
+package io.dargenn.quizhouse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping(value = "/")
+    public String index(Model model) {
+        System.out.println("fhuewhiufhw");
+        model.addAttribute("tekst", "jkefhwiuhfiewhiufhiwh");
+        return "index";
+    }
+}
---
+++ src/main/java/io/dargenn/quizhouse/QuizhouseApplication.java
@@ -0,0 +1,12 @@
+package io.dargenn.quizhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizhouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizhouseApplication.class, args);
+    }
+}
