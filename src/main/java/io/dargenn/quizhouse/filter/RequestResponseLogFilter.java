88f8ca247519f8d4a67a8ecb4de1945132f2e132
Michał Dargiewicz <michaldargiewicz1@gmail.com>
2018-10-20 09:23:02 +0200

    spring filter

---
+++ src/main/java/io/dargenn/quizhouse/filter/RequestResponseLogFilter.java
@@ -0,0 +1,32 @@
+package io.dargenn.quizhouse.filter;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(1)
+public class RequestResponseLogFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest,
+                         ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
